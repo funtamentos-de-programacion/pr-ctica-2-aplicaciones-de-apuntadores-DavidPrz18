f013d7320b30bdc99b72baf27930c4dbefa4399d
Perez Cruz Jorge David <jorgeedavidpc14@gmail.com>
2025-02-20 11:01:13 -0600

    archivos que se tranbajaron en la practica

--- ejercicio2.c
+++ ejercicio2.c
@@ -18,20 +18,24 @@ int main () {
   // suma una localidad al inicio del arreglo e imprime su valor
   printf("*(apArr+1) = %i\n",*(apArr+1));
   printf("*(apArr+2) = %i\n",*(apArr+2));
+  printf("*(apArr+1) = %i\n",*(apArr+3));
+  printf("*(apArr+2) = %i\n",*(apArr+4));
  // Escribe una línea de código para mostrar el valor apuntado por apArr
  
   printf("Recorriendo el arreglo usando un apuntador:\n");
   for (int i = 0; i < 5; i++) {
-    printf("Elemento %d: %d\n", i, *(ptr + i));
+    printf("Elemento %d: %d\n", i, *(apArr + i));
   }
 
   //Modifica apArr para que apunte al tercer elemento
 
   //Accede al elemento con indice -2 usando el apuntador
-
+  printf("Elemento -2: %d\n", i, *(apArr[-2]));
+  printf("Elemento 0: %d\n", i, *(apArr[0]));
   //Ajusta el ciclo for para que se pueda recorrer completo el arreglo de acuerdo al elemento apuntado
-  for (int i = ??; i < ??; ???) {
-    printf("Elemento %d: %d\n", i, *(ptr + i));
+  for (int i = -3; i < 2; i++) {
+    printf("Elemento %d: %d\n", i, *(apArr + i));
+    
   }
   return 0;
 }
