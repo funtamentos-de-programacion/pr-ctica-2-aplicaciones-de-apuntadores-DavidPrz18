ee30b93cfb506e5a2f60cc02e5b3f1eef0a2b31f
Pérez Cruz Jorge David <jorgeedavidpc14@gmail.com>
2025-03-01 09:12:32 -0600

    ejercicio 7 de la práctica 2

--- ejercicio7.c
+++ ejercicio7.c
@@ -1,8 +1,14 @@
 // 7. Accediendo a la Memoria
+// Accediendo a la Memoria: Escribe un programa que imprima la dirección de memoria de cada elemento de un arreglo de enteros.
 #include <stdio.h>
+
 
 int main() {
     int arr[5] = {10, 20, 30, 40, 50};
     // Código para imprimir direcciones de memoria aquí
+
+    for (int i = 0; i < 5; i++) {
+        printf("Dirección de memoria de arr[%d]: %p\n", i, &arr[i]);
+    }
     return 0;
 }
