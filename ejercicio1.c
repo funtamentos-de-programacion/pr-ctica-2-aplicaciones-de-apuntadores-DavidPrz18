1d464e329f0ebc30ec8be6e89b5c496881811dd1
Pérez Cruz Jorge David <jorgeedavidpc14@gmail.com>
2025-03-01 07:19:03 -0600

    ejercicio 1 de la práctica2

--- ejercicio1.c
+++ ejercicio1.c
@@ -6,31 +6,31 @@ int main () {
   d = 'z';
   e = '\t';
   
-  //Escrib el código para imprimir las direcciones de c, d y e
+  //Escribe el código para imprimir las direcciones de c, d y e
 
   ap = &c;
   // imprime el carácter de la localidad a la que apunta
-  printf("Carácter: %c\n",*ap);
+  printf("Caracter: %c\n",*ap);
   // imprime el código ASCII de la localidad a la que apunta
-  printf("Código ASCII: %d\n",*ap);
+  printf("Codigo ASCII: %d\n",*ap);
   // imprime la dirección de memoria de la localidad a la que apunta
-  printf("Dirección de memoria: %d\n",ap);
+  printf("Direccion de memoria: %p\n",ap);
 
   ap = &d;
   // imprime el carácter de la localidad a la que apunta
-  printf("Carácter: %c\n",*ap);
+  printf("Caracter: %c\n",*ap);
   // imprime el código ASCII de la localidad a la que apunta
-  printf("Código ASCII: %d\n",*ap);
+  printf("Codigo ASCII: %d\n",*ap);
   // imprime la dirección de memoria de la localidad a la que apunta
-  printf("Dirección de memoria: %d\n",ap);
+  printf("Direccion de memoria: %p\n",ap);
 
   ap = &e;
   // imprime el carácter de la localidad a la que apunta
-  printf("Carácter: %c\n",*ap);
+  printf("Caracter: %c\n",*ap);
   // imprime el código ASCII de la localidad a la que apunta
-  printf("Código ASCII: %d\n",*ap);
+  printf("Codigo ASCII: %d\n",*ap);
   // imprime la dirección de memoria de la localidad a la que apunta
-  printf("Dirección de memoria: %d\n",ap);
+  printf("Direccion de memoria: %p\n",ap);
 
  return 0;
 }
