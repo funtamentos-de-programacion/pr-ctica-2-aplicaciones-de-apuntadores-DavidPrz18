bf22d2164b1642db1ae87becb718f356d1bf8c98
Pérez Cruz Jorge David <jorgeedavidpc14@gmail.com>
2025-03-01 08:31:08 -0600

    ejericio 5 de la práctica 2

--- ejercicio5.c
+++ ejercicio5.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-
+//este programa muestra la diferencia entre pasar un valor y pasar una referencia con ayuda de dos funciones
 void pasarValor(int);
 void pasarReferencia(int *);
 
@@ -18,13 +18,13 @@ int main(){
   return 0;
 }
 
-
+// en esta función se recibe un valor y se modifica, pero no afecta el valor en main ()
 void pasarValor(int equis){
   printf("%d\n", equis);
   equis = 128;
   printf("%d\n", equis);
 }
-
+// en esta función se recibe un valor y se modifica, aquí sí se afecta el valor en main ()
 void pasarReferencia(int *equis){
   printf("%d\n", *equis);
   *equis = 128;
