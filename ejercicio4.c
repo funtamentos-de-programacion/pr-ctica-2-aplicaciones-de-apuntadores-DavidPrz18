e4af0ff2a7fc6ad892edb99e6a7f8826acdec7e4
Pérez Cruz Jorge David <jorgeedavidpc14@gmail.com>
2025-03-01 07:23:29 -0600

    ejercicio 4 de la práctica 2

--- ejercicio4.c
+++ ejercicio4.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 
 int main() {
-  int arr[] = {1, 2, 3, 4, 5};
+  int arr[ ] = {1, 2, 3, 4, 5};
   int *ptr = arr;
   
   printf("Valores originales:\n");
   for (int i = 0; i < 5; i++) {
     //Escribe el codigo para imprimir los valores usado el apuntador
+   printf("%d ", *(ptr+i));
   }
-  
-  for (int i = 0; i < 5; i++) {
-    // Duplicar cada elemento
-  }
-  
-  printf("\nValores modificados:\n");
-  for (int i = 0; i < 5; i++) {
-    //Escribe el codigo para imprimir los valores usado el apuntador
-  }
-  return 0;
+  printf("\n");
+
+   // Duplicar cada elemento
+ for(int i = 0; i < 5; i++) {
+  *(ptr+i) *= 2; //*(ptr+i)= *(ptr+1)*2
+ }
+
+  printf("Valores modificados:\n");
+
+  //Escribe el codigo para imprimir los valores usado el apuntador
+ for(int i=0; i<5; i++){
+  printf("%d ", *(ptr+i));
+ }
+ 
+printf("\n");
+
+return 0;
 }
