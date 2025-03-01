f434734f7c2f1ac3583a6205daef601e2a8cbf7c
Pérez Cruz Jorge David <jorgeedavidpc14@gmail.com>
2025-03-01 07:20:29 -0600

    ejercicio 2 de la práctica 2

--- ejercicio2.c
+++ ejercicio2.c
@@ -7,11 +7,11 @@ int main () {
   short arr[5] ={1,2, 3, 4, 5}, *apArr;
   apArr = &arr[0];
   // imprime la dirección de memoria del arreglo en la posición [0]
-  printf("Dirección del arreglo en la primera posición: %x\n",&arr[0]);
+  printf("Direccion del arreglo en la primera posicion: %x\n",&arr[0]);
   // imprime la dirección de memoria del arreglo  (el nombre del arreglo es un apuntador)
-  printf("Dirección del arreglo: %x\n",&arr);
+  printf("Direccion del arreglo: %x\n",&arr);
   // imprime la dirección de memoria almacenada en el apuntador apArr
-  printf("Dirección almacenada en el apuntador: %x\n",apArr);
+  printf("Direccion almacenada en el apuntador: %x\n",apArr);
 
 
   printf("*apArr = %i\n",*apArr);
@@ -22,17 +22,21 @@ int main () {
   printf("*(apArr+2) = %i\n",*(apArr+4));
  // Escribe una línea de código para mostrar el valor apuntado por apArr
  
-  printf("Recorriendo el arreglo usando un apuntador:\n");
+  printf("Recorriendo el arreglo usando un for:\n");
   for (int i = 0; i < 5; i++) {
     printf("Elemento %d: %d\n", i, *(apArr + i));
   }
 
-  //Modifica apArr para que apunte al tercer elemento
-
+  //Modifica apArr para que apunte al cuarto elemento
+apArr = &arr[3];
   //Accede al elemento con indice -2 usando el apuntador
-  printf("Elemento -2: %d\n", i, *(apArr[-2]));
-  printf("Elemento 0: %d\n", i, *(apArr[0]));
+  printf("el apuntador ahora apunta al cuarto elemento\n");
+  printf("Elemento -2: %d\n", (apArr[-2]));
+  printf("Elemento 0: %d\n", apArr[0]);
+  printf("Elemento -3: %d\n", apArr[-3]);
+
   //Ajusta el ciclo for para que se pueda recorrer completo el arreglo de acuerdo al elemento apuntado
+  printf("ahora se muestra el arreglo completo de acuerdo con esta modificacion \n");
   for (int i = -3; i < 2; i++) {
     printf("Elemento %d: %d\n", i, *(apArr + i));
     
